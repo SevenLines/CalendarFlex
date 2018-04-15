d64f170a67075b39b38bd41a692e008022ecd17f
m <mmailm@mail.ru>
2018-04-15 14:54:50 +0800

    add buttons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <calendar></calendar>
+  <div class="wrapper">
+    <div class="content">
+      <div class="header">
+        <button @click="--year">&lt;</button>
+        <div class="title">{{year}}</div>
+        <button @click="++year">&gt;</button>
+      </div>
+      <div class="body">
+        <calendar :year="year"></calendar>
+      </div>
+      <div class="header">
+        <div class="title">{{year}}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,11 +21,50 @@ export default {
   name: 'app',
   data () {
     return {
+      year: 2018
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  html {
+    height: 100%;
+  }
 
+  body {
+    height: 100%;
+    margin: 0;
+  }
+</style>
+
+<style lang="scss" scoped>
+  .title {
+    font-weight: bold;
+    font-size: 1.5em;
+    margin: 0.25em;
+    text-align: center;
+  }
+
+  .header {
+    padding: 0.25em;
+    display: flex;
+    flex-shrink: 0;
+    justify-content: space-between;
+  }
+
+  .wrapper { // ограничивает высоту
+    display: flex;
+    height: 100%;
+  }
+
+  .content { // непосредственный контейнер с заголовком и содержимым
+    display: flex;
+    flex-direction: column;
+  }
+
+  .body { // основное тело контейнер
+    flex-grow: 1; // растягивается чтобы заполнить  все пространство
+    overflow-y: auto; // скролл если не влезает
+  }
 </style>
--- src/Calendar.vue
+++ src/Calendar.vue
@@ -16,7 +16,7 @@
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .title {
     margin: 0.25em;
     font-weight: bold;
